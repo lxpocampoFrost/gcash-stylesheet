734d24b5ad37231435b7abf9e514bf88d615af67
lxpocampoFrost <117623161+lxpocampoFrost@users.noreply.github.com>
2023-04-14 17:50:54 +0800

    Update app.js

--- assets/partners/app.js
+++ assets/partners/app.js
@@ -8,23 +8,23 @@
 const createItem = (item, templateElement) => {
 
 
-  // Clone the template element
-  const newItem = templateElement.cloneNode(true);
-  newItem.classList.remove('hidden');
+    // Clone the template element
+    const newItem = templateElement.cloneNode(true);
+    newItem.classList.remove('hidden');
 
-  // Query inner elements
-  const name = newItem.querySelector('.biller-item-title');
-  const payment_date = newItem.querySelector('.biller-item-description');
-  const gcredit = newItem.querySelector('[data-element="gcredit"]');
-  const fee = newItem.querySelector('[data-element="gcredit"]');
-
-
-  // Populate inner elements
-  if (name) name.textContent = item.name || item.Name;
-  if (payment_date) payment_date.textContent = item.payment_date;
+    // Query inner elements
+    const name = newItem.querySelector('.biller-item-title');
+    const payment_date = newItem.querySelector('.biller-item-description');
+    const gcredit = newItem.querySelector('[data-element="gcredit"]');
+    const fee = newItem.querySelector('[data-element="gcredit"]');
 
 
-  return newItem;
+    // Populate inner elements
+    if (name) name.textContent = item.name || item.Name;
+    if (payment_date) payment_date.textContent = item.payment_date;
+
+
+    return newItem;
 };
 
 /**
@@ -32,15 +32,15 @@ const createItem = (item, templateElement) => {
 * @method fetchPartners() Returns Array from API that uses JSON
 */
 async function fetchPartners(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    return data;
-  } catch (error) {
-    console.log('fetchPartners(): ', error.message);
-    return [];
-  }
+        return data;
+    } catch (error) {
+        console.log('fetchPartners(): ', error.message);
+        return [];
+    }
 };
 /**
 * Creates an item from the template element.
@@ -50,58 +50,98 @@ async function fetchPartners(url) {
 * @returns A collection of dom elements.
 */
 function renderItems(results_area, filter_data, template_element) {
-  //Clear the children of results
-  results_area.textContent = '';
+    //Clear the children of results
+    results_area.textContent = '';
 
-  //Get converted items
-  let newItems = filter_data.map((item) => createItem(item, template_element));
+    //Get converted items
+    let newItems = filter_data.map((item) => createItem(item, template_element));
 
-  //Append new filtered itesm to HTML
-  newItems.map((item) => results_area.append(item));
+    //Append new filtered itesm to HTML
+    newItems.map((item) => results_area.append(item));
 
-  results_area.animate(
-    {
-      opacity: [0, 1]
-    },
-    300
-  )
+    results_area.animate(
+        {
+            opacity: [0, 1]
+        },
+        300
+    )
 }
 
 
+
 (async function () {
-  const template_element = document.querySelector('.biller-result-card.hidden');
-  const results_area = document.querySelector('.biller-result');
-  const search_input = $('#field-2');
+    const template_element = document.querySelector('.biller-result-card.hidden');
+    const results_area = document.querySelector('.biller-result.billers_collection-list');
+    const search_input = $('#field-2');
+    const pagination_container = $('#pagination-area');
+    let arrowSVG = `<svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="20.5" cy="20.4922" r="19" stroke="#025AE9" stroke-width="2"/>
+    <path d="M19 24L23 20L19 16" stroke="#025AE9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
 
-  results_area.style.opacity = '1';
-  //Get the data from URL source
-  let partnersData = await fetchPartners('https://lxpocampofrost.github.io/gcash-stylesheet/assets/partners.json');
-  //Initialize an empty array
-  let filterd_items = [];
+    console.log(results_area);
 
-  //Converts data to node elements
-  let domItems = partnersData.map((item) => createItem(item, template_element));
+    results_area.style.opacity = '1';
+    //Get the data from URL source
+    let partnersData = await fetchPartners('https://lxpocampofrost.github.io/gcash-stylesheet/assets/partners.json');
+    //Initialize an empty array
+    let filterd_items = [];
 
-  //Appends newly converted elements to results area
-  domItems.map((item) => results_area.append(item));
+    //Converts data to node elements
+    // let domItems = partnersData.map((item) => createItem(item, template_element));
 
-  //Input search functionality
-  search_input.on("input", function () {
-    let inputValue = $(this).val().toLowerCase();
+    //Appends newly converted elements to results area
+    // domItems.map((item) => results_area.append(item));
 
-    filterd_items = partnersData.filter((item) =>
-      item.name.toLowerCase().includes(inputValue)
-    );
+    //Input search functionality
+    search_input.on("input", function () {
+        let inputValue = $(this).val().toLowerCase();
 
-    renderItems(results_area, filterd_items, template_element);
-  });
+        filterd_items = partnersData.filter((item) =>
+            item.name.toLowerCase().includes(inputValue)
+        );
+        // updatePagination(filterd_items);
+        // renderItems(results_area, filterd_items, template_element);
+        pagination_container.pagination('destroy');
+        pagination_container.pagination({
+            ulClassName: 'pagination-list-wrapper',
+            dataSource: filterd_items,
+            prevText: arrowSVG,
+            nextText: arrowSVG,
+            pageSize: 15,
+            callback: function (data, pagination) {
+                console.log(pagination);
+                // template method of yourself
+                renderItems(results_area, data, template_element);
+            }
+        })
+
+    });
+
+    pagination_container.pagination({
+        ulClassName: 'pagination-list-wrapper',
+        dataSource: partnersData,
+        prevText: arrowSVG,
+        nextText: arrowSVG,
+        pageSize: 15,
+        callback: function (data, pagination) {
+
+            // template method of yourself
+            renderItems(results_area, data, template_element);
+        }
+    })
 
 
-  //Create pagination
+    /*
+        ToDo: 
+        - Search input and billers dropdown should filter from filtered_items
+        - Pagination gets from filtered items
+    */
 
 
+    //Create pagination
 })();
-
 
 
 
